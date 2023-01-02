2dda4879b4ba7ca194154469bca7ba499dd99bd2
CactusDad <buoya88mosli88@gmail.com>
2023-01-02 23:54:42 +0100

    paser of ex00 day06

--- day06/ex00/main.cpp
+++ day06/ex00/main.cpp
@@ -1,9 +1,12 @@
+
+
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <cfloat>
 #include <iomanip>
 #include <climits>
+#include <cstring>
 void character(char c , double flag)
 {
     if ( flag > 255 || flag < 0
@@ -35,21 +38,70 @@ void con_double(double num)
   std::cout << "double: " << num << std::endl;
 }
 
+bool checknum(char *str)
+{
+  int i;
+  int pre;
+  i = 0;
+  pre = 0;
+  
+  if (str[i] == '+' || str[i] == '-')
+    i++;
+  while ((isdigit(str[i]) || str[i] == '.') && pre < 2)
+  {
+    if (str[i] == '.')
+      pre++;
+    i++;
+  }
+  if (str[i] == 'f' && pre < 2)
+          i++;
+
+  return (i == strlen(str));
+}
+
+bool data_words(char *str)
+{
+  int i;
+  std::string dataWord = std::string(str);
+  std::string words[12] = {
+    "nan","-nan", "+nan", "nanf" , "-nanf", "+nanf",
+    "inf","-inf", "+inf", "inff", "-inff" , "+inff"};
+  i = 0;
+  while (i < 12 && dataWord.compare(words[i]))
+    i++;
+
+  return (i != 12);
+
+}
+double checkinput(char *str)
+{
+  if (isprint(*str) && !isdigit(*str) && strlen(str) == 1)
+      return (static_cast<double>(*str));      
+  else if  (!checknum(str) && !data_words(str))
+  {
+      std::cout << "Validation of input field" << std::endl;
+      exit(1);
+  }
+
+  return (static_cast<double>(std::atof(str)));
+}
 int main(int argc, char *argv[])
 {
   double number;
   
   if (argc == 2)
-  {
-    number = ::atof(argv[1]);
-    
-    character(static_cast<char>(number), number);
-    integer(static_cast<int>(number), number);
-    con_float(static_cast<float>(number), number);
-    con_double(static_cast<double>(number));
-    
-  }
+      {
+	std::cout << "======================================================" << std::endl;
+	number = checkinput(argv[1]);
+	std::cout << "number: "<< number << std::endl;
+	
+	character(static_cast<char>(number), number);
+	integer(static_cast<int>(number), number);
+	con_float(static_cast<float>(number), number);
+	con_double(static_cast<double>(number));
+      }
   else
     std::cout << "[program name] [string parameter]" << std::endl;
   return (0);
+
 }
