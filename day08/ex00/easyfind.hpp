a87f3a9e81ffbe11c8591538294b4d3d4bcdaf10
CactusDad <buoya88mosli88@gmail.com>
2023-01-06 23:53:39 +0100

    day08 ex00 implement easyfind function with STL

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -5,5 +5,12 @@
 #include <iostream>
 
 template <typename T>
-
+int easyfind(T contianer, int number)
+{
+  typename T::iterator it;
+  it = find(contianer.begin(), contianer.end(), number);
+  if (it == contianer.end())
+    throw ("the element not found");
+  return *it ;
+}
 #endif
--- day08/ex00/main.cpp
+++ day08/ex00/main.cpp
@@ -1,9 +1,33 @@
 
 #include <iostream>
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
 
 int main()
 {
+  std::vector<int> ar;
+  std::list<int> listo;
 
-  std::cout << "hello world" << std::endl;
+  ar.push_back(1);
+  ar.push_back(2);
+  ar.push_back(3);
+  ar.push_back(4);
+
+  listo.push_back(2);
+  listo.push_back(3);
+  listo.push_back(4);
+  try
+    {
+      int go = easyfind(ar, 3);
+      std::cout << "the return: " << go << std::endl;
+      int li = easyfind(listo,4);
+      std::cout << "the easyfind found the number: " << li << std::endl;
+      go = easyfind(listo, 1);
+    }
+  catch(char const *str)
+    {
+      std::cout << str << std::endl;
+    }
   return (0);
 }
